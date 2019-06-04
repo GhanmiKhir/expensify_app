23a3472a5528f7650c7fc7f2585bdd39834f167e
GhanmiKhir <ghanmikhair@live.fr>
2019-06-04 17:19:04 +0100

    Fixed typo

--- src/tests/componenets/ExpenseListItem.test.js
+++ src/tests/componenets/ExpenseListItem.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import expenses from '../fixtures/expenses';
 import { ExpenseListItem } from '../../components/ExpenseListItem';
 
-test('Should render the expense list item', () => {
+test('should render the expense list item', () => {
   const wrapper = shallow(<ExpenseListItem {...expenses[1]} />);
   expect(wrapper).toMatchSnapshot();
 });
