e798b0330f3c171101268a03f47ffc0408fa8dfa
GhanmiKhir <ghanmikhair@live.fr>
2019-06-05 10:40:38 +0100

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -3,26 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  console.log(visibleExpenses);
-});
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 5000 }));
-store.dispatch(
-  addExpense({ description: 'Gas bill', amount: '2000', createdAt: 1000 })
-);
-store.dispatch(addExpense({ description: 'Rent', amount: 1059500 }));
 
 const jsx = (
   <Provider store={store}>
