241bd6ca4e0780713f4141ab3e2c41c59a48f702
GhanmiKhir <ghanmikhair@live.fr>
2019-06-08 13:19:13 +0100

    Add startEditExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,4 +1,3 @@
-import uuid from 'uuid';
 import database from '../firebase/firebase';
 
 // ADD_EXPENSE
@@ -55,6 +54,18 @@ export const editExpense = (id, updates) => ({
   updates
 });
 
+export const startEditExpense = (id, updates) => {
+  return dispatch => {
+    return database
+      .ref(`expenses/${id}`)
+      .update(updates)
+      .then(() => {
+        dispatch(editExpense(id, updates));
+      });
+  };
+};
+
+// SET_EXPENSES
 export const setExpenses = expenses => ({
   type: 'SET_EXPENSES',
   expenses
