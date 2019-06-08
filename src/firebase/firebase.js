28d3170ff9689d119edbd7c1236d45dc0a7f108c
GhanmiKhir <ghanmikhair@live.fr>
2019-06-08 11:49:32 +0100

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@ import * as firebase from 'firebase';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCBxo7yicbeL0RqHU4Smi8HzS24QwROWbY',
-  authDomain: 'expensify-78e2b.firebaseapp.com',
-  databaseURL: 'https://expensify-78e2b.firebaseio.com',
-  projectId: 'expensify-78e2b',
-  storageBucket: 'expensify-78e2b.appspot.com',
-  messagingSenderId: '609587326858',
-  appId: '1:609587326858:web:b7049e061134f3e0'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
@@ -16,9 +16,11 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database.ref().on('value', snapshot => {
-  console.log(snapshot.val());
-});
+export { firebase, database as default };
+
+// database.ref().on('value', snapshot => {
+//   console.log(snapshot.val());
+// });
 
 // database
 //   .ref('location/city')
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,6 +1,11 @@
 import Enzype from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import dotenv from 'dotenv';
 
 Enzype.configure({
   adapter: new Adapter()
+});
+
+dotenv.config({
+  path: '.env.test'
 });
